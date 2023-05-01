58d28334a2a0dd15f3d0bb0f9629b0f3673bf1a6
Totoro700 <ianfrancisko@gmail.com>
2023-04-30 22:09:53 -0500

    more updates

--- crypter.py
+++ crypter.py
@@ -1,36 +1,47 @@
 import os, time, sys, string
 
-def t(msg):
-    for i in msg:
-        sys.stdout.flush()
-        sys.stdout.write(i)
-        time.sleep(0.02)
-    print()
-           
-
 def fileCheck(fn):
     return os.path.isfile(fn)
 
 def writeOutput(str: list):
     f = open('output.txt', 'w')
-    open.write(str)
+    f.write(str)
     f.close()
 
+def switch(str: list, alph: list):
+    strLen = len(str)
+    a = 0
+    temp = []
+    print(strLen)
+    while a < strLen:
+        if str[a] == string.punctuation || str[a] == " ":
+            
+        toAppend = alph[]
+        temp.append(toAppend)
+        a=a+1
+        print(a)
+    return ''.join(temp)
+    #make array of numbers into letters and return
 
 def crypt(str: list):
     crypted = []
-    alph = list(string.ascii_letters)
+    alph = list(string.ascii_letters + string.digits + string.punctuation + " ")
+    temp = []
+    cryptedNums = []
     for i in str:
-                
+        temp.append(alph.index(i))
+    cryptedNums = [x+1 for x in temp]
+    crypted = switch(cryptedNums, alph)
+    writeOutput(crypted)
 
 def loop():
     filename = input('File name (exact directory like "example.txt") ')
     if fileCheck(filename):
         file = open(filename, 'r')
-        t(crypt(list(file.read())))
+        print(crypt(list(file.read())))
         file.close()
     else:
-        t('Invalid file name try again')
+        print('Invalid file name try again')
 
 while True:
     loop()
