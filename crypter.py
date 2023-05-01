67e1e4d5d6c6a898f05a297adfb254c915cd0160
Totoro700 <ianfrancisko@gmail.com>
2023-04-30 22:23:55 -0500

    another attempt

--- crypter.py
+++ crypter.py
@@ -12,27 +12,29 @@ def switch(str: list, alph: list):
     strLen = len(str)
     a = 0
     temp = []
-    print(strLen)
+    alph = alph.append(" ")
     while a < strLen:
-        if str[a] == string.punctuation || str[a] == " ":
-            
-        toAppend = alph[]
-        temp.append(toAppend)
-        a=a+1
-        print(a)
+        try: 
+            temp.append(alph[str[a]])
+            a=a+1
+        except: 
+            temp.append("")
+            a=a+1
+        #print(a)
+    print(''.join(temp))
     return ''.join(temp)
     #make array of numbers into letters and return
 
 def crypt(str: list):
     crypted = []
-    alph = list(string.ascii_letters + string.digits + string.punctuation + " ")
+    alph = list(string.ascii_letters + string.digits + string.punctuation + " " + string.whitespace)
     temp = []
     cryptedNums = []
     for i in str:
         temp.append(alph.index(i))
-    cryptedNums = [x+1 for x in temp]
-    crypted = switch(cryptedNums, alph)
-    writeOutput(crypted)
+    cryptedNums = [x+18 for x in temp]
+    #print(cryptedNums)
+    writeOutput(switch(cryptedNums, alph))
 
 def loop():
     filename = input('File name (exact directory like "example.txt") ')
